32a1bed22b114c9444842918de8f909fa70f0c93
IPogorelova <pisseversk@mail.ru>
2018-01-24 14:00:41 +0300

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,31 @@
+import re
+import os
+import xml.etree.ElementTree as ET
+
+
+def parseXML(dir):
+    files_list = os.listdir(dir)
+    orders = []
+
+    for file_name in files_list:
+        file_path = dir + '\\' + file_name
+        tree = ET.parse(file_path)
+
+        root = tree.getroot()
+
+        for element in root:
+            amount = element.attrib['Amount']
+            content_data = element.attrib['ContentData']
+            paper_size = re.search('\d?\d\dx\d?\d?\d\d', content_data).group(0).split('x')
+
+            order_parameters = [int(paper_size[0]), int(paper_size[1]), int(amount)]
+
+        orders.append(order_parameters)
+
+    print(orders)
+    return orders
+
+
+parseXML('C:\\Users\\Инна\\Desktop\\Диплом\\SD_02856\\test')
+
+
