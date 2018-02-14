f6575beffc45d79908ba3cca408d61400d5d65ae
IPogorelova <pisseversk@mail.ru>
2018-02-14 21:48:49 +0300

    Update FFDH_algorithm.py

--- FFDH_algorithm.py
+++ FFDH_algorithm.py
@@ -35,73 +35,71 @@ def pack(item_list, max_width):                             # max_width - зад
 
     new_level = Level()
     new_level.append(item_list[0])
-    # new_level.height = lambda item_list: item[1]
-    # new_level_width = lambda item_list: item[0]
-    # new_level.width = new_level_width
-    levels_list.append(new_level)
+    levels_list.append(['level1', new_level, max_width])            # [номер уровня, уровень, свободное место на уровне]
     level_counter = 1
 
     for item in item_list:
         # Try to fit item into a Level
         level_num = str('level' + str(level_counter))
-        min_free_space = max_width
 
         for level in levels_list:
-            free_space = max_width - (new_level.width + item[0])
+            free_space = level[2] - item[0]
+                                                                    # min_free_space = max_width
+            if free_space > 0:                                      # if all([(free_space > 0), (free_space <= min_free_space)]): - for BFDH_alghorithm
+                new_level.append(item)                              # min_free_space = free_space
+                level[2] = free_space
+            else:
+                # контейнер не входит ни на один уровень - создаём новый
+                print('Level ' + level_num + ' is full. Creating a new level.')
+                levels_list.append([level_num, new_level, (free_space + item[0])])          # добавляет старый уровень в список уровней
+                new_level = Level()
+                new_level.append(item)
+                new_level.height = item[1]
+                new_level.width = item[0]
+                level[2] = (841 - item[0])
 
-            if free_space <= min_free_space:
-                min_free_space = free_space
-                if free_space < max_width:
-                    new_level.append(item)
-                else:
-                    # контейнер не входит ни на один уровень - создаём новый
-                    print('Level ' + level_num + ' is full. Creating a new level.')
-                    new_level.append(levels_list)           # добавляет старый уровень в список уровней
-                    new_level = Level()
-                    new_level.append(item)
-                    new_level.height = item[1]
-                    new_level.width = item[0]
-                    levels_list.append(new_level)
-                    level_counter += 1
+                level_counter += 1
+                break
 
             # levels_height_sum += new_level.height
 
     return levels_list
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
+#
+#    def packAndShow(aList, maxWidth, maxHeight): # aList - здесь orders, maxValue - здесь формат листа бумаги
+#        """ Pack a list into bins and show the result """
+#
+#        papers_list = []
+#        new_paper = Paper(maxHeight, maxWidth)
+#        papers_list.append(new_paper)
+#
+#        levels_list = pack(aList, maxWidth)
+#        level_counter = len(levels_list)
+#
+#        for new_paper in papers_list:
+#            levels_height_sum = 0
+#            paper_counter = 1
+#            for level in levels_list:
+#                levels_height_sum += level.height
+#
+#                if new_paper.height + levels_height_sum <= maxHeight:
+#                    new_paper.append_level(level)
+#                else:
+#                    papers_list.append(new_paper)
+#                    new_paper = Paper(maxHeight, maxWidth)
+#                    new_paper.append_level(level)
+#                    paper_counter += 1
 
-    def packAndShow(aList, maxWidth, maxHeight): # aList - здесь orders, maxValue - здесь формат листа бумаги
-        """ Pack a list into bins and show the result """
+#        print('List with orders requires', level_counter, 'levels')
+#        print('All levels require ', paper_counter, ' pieces of paper')
 
-        papers_list = []
-        new_paper = Paper(maxHeight, maxWidth)
-        papers_list.append(new_paper)
-
-        levels_list = pack(aList, maxWidth)
-        level_counter = len(levels_list)
-        print('List with orders requires', level_counter, 'levels')
-
-        for new_paper in papers_list:
-            levels_height_sum = 0
-            paper_counter = 1
-            for level in levels_list:
-                levels_height_sum += level.height
-
-                if new_paper.height + levels_height_sum <= maxHeight:
-                    new_paper.append_level(level)
-                else:
-                    papers_list.append(new_paper)
-                    new_paper = Paper(maxHeight, maxWidth)
-                    new_paper.append_level(level)
-                    paper_counter += 1
-                    
-        print('List with orders requires', level_counter, 'levels')
-        print('All levels require ', paper_counter, ' pieces of paper')
-
-    for level in levels_list:
-        print('Level', level_counter, ':/n', level)
+#    for level in levels_list:
+#        print('Level', level_counter, ':/n', level)
 
 
-    itemList = my_parser.parseXML('C:\\Users\\Инна\\Desktop\\Диплом\\SD_02856\\test')
-    packAndShow(itemList, 841, 1189)
+itemList = my_parser.parseXML('C:\\Users\\Инна\\Desktop\\Диплом\\SD_02856\\test')
+#print(itemList)
+#packAndShow(itemList, 841, 1189)
+pack(itemList, 841)
