dd18bfb3f2aa744c5ef17e9f607a9d4170ececc0
IPogorelova <pisseversk@mail.ru>
2018-03-11 21:32:22 +0300

    Update FFDH_algorithm.py

--- FFDH_algorithm.py
+++ FFDH_algorithm.py
@@ -9,41 +9,44 @@ class Paper(object):
         self.width = width
         self.items = []
 
-    def append_level(self, level):             # добавляем все Level на лист Paper
+    def append_level(self, level):                                  # добавляем все Level на лист Paper
         self.items.append(level)
 
+    def __str__(self):
+        """ Printable representation """
+        return 'Levels on paper: %s' % (str(self.items))
 
 class Level(Paper):
     """ Level in each container """
     def __init__(self):
         Paper.__init__(self, height=0, width=0)
 
-    def append(self, item):             # добавляем item на уровень (Level)
+    def append(self, item):                                         # добавляем item на уровень (Level)
         self.items.append(item)
         self.width += item[0]
         self.height += item[1]
 
-    # def __str__(self):
-    #    """ Printable representation """
-    #    return 'Bin(sum=%d, items=%s)' % (self.sum, str(self.items))
+    def __str__(self):
+        """ Printable representation """
+        return 'Items on level: %s; level_width: %d; level_height: %d' % (str(self.items), self.width, self.height)
 
 
-def pack(item_list, max_width):                             # max_width - задаётся вручную пока, ширина нынешнего формата
+def pack(item_list, max_width):                                     # max_width - задаётся вручную пока, ширина нынешнего формата
     # функция просто упаковки в контейнер и создания нового при нехватке места
-    sorted(item_list, key=lambda i: i[1], reverse=True)
+    item_list.sort(key=lambda i: i[1], reverse=True)
     levels_list = []
 
     new_level = Level()
-    new_level.append(item_list[0])
-    levels_list.append(['level1', new_level, max_width])            # [номер уровня, уровень, свободное место на уровне]
+    #new_level.append(item_list[0])
+    levels_list.append(['level_1', new_level, max_width])            # [номер уровня, уровень, свободное место на уровне]
     level_counter = 1
 
     for item in item_list:
         # Try to fit item into a Level
-        level_num = str('level' + str(level_counter))
+        level_num = str('level_' + str(level_counter))
 
         for level in levels_list:
-            free_space = level[2] - item[0]
+            free_space = level[2] - item[0]                         # для BFDH
                                                                     # min_free_space = max_width
             if free_space > 0:                                      # if all([(free_space > 0), (free_space <= min_free_space)]): - for BFDH_alghorithm
                 new_level.append(item)                              # min_free_space = free_space
@@ -53,12 +56,12 @@ def pack(item_list, max_width):                             # max_width - зад
                 print('Level ' + level_num + ' is full. Creating a new level.')
                 levels_list.append([level_num, new_level, (free_space + item[0])])          # добавляет старый уровень в список уровней
                 new_level = Level()
+                level_counter += 1
+
                 new_level.append(item)
                 new_level.height = item[1]
                 new_level.width = item[0]
                 level[2] = (841 - item[0])
-
-                level_counter += 1
                 break
 
             # levels_height_sum += new_level.height
@@ -66,40 +69,41 @@ def pack(item_list, max_width):                             # max_width - зад
     return levels_list
 
 
-#if __name__ == '__main__':
-#
-#    def packAndShow(aList, maxWidth, maxHeight): # aList - здесь orders, maxValue - здесь формат листа бумаги
-#        """ Pack a list into bins and show the result """
-#
-#        papers_list = []
-#        new_paper = Paper(maxHeight, maxWidth)
-#        papers_list.append(new_paper)
-#
-#        levels_list = pack(aList, maxWidth)
-#        level_counter = len(levels_list)
-#
-#        for new_paper in papers_list:
-#            levels_height_sum = 0
-#            paper_counter = 1
-#            for level in levels_list:
-#                levels_height_sum += level.height
-#
-#                if new_paper.height + levels_height_sum <= maxHeight:
-#                    new_paper.append_level(level)
-#                else:
-#                    papers_list.append(new_paper)
-#                    new_paper = Paper(maxHeight, maxWidth)
-#                    new_paper.append_level(level)
-#                    paper_counter += 1
+if __name__ == '__main__':
 
-#        print('List with orders requires', level_counter, 'levels')
-#        print('All levels require ', paper_counter, ' pieces of paper')
+    def packAndShow(aList, maxWidth, maxHeight):                    # aList - здесь orders, maxValue - здесь формат листа бумаги
+        """ Pack a list into bins and show the result """
 
-#    for level in levels_list:
-#        print('Level', level_counter, ':/n', level)
+        papers_list = []
+        new_paper = Paper(0, 0)
+        papers_list.append(new_paper)
+        levels_list = pack(aList, maxWidth)
+        level_counter = len(levels_list)
+        print('List with orders requires', level_counter, 'levels')
+
+        levels_height_sum = 0
+        paper_counter = 1
+        for element in levels_list:
+            level = element[1]
+            levels_height_sum += level.height
+
+            for new_paper in papers_list:
+                if new_paper.height + levels_height_sum <= maxHeight:
+                    new_paper.append_level(level)
+                    break
+                else:
+                    papers_list.append(new_paper)
+                    new_paper = Paper(0, 0)
+                    new_paper.append_level(level)
+                    levels_height_sum = 0
+                    paper_counter += 1
+                    break
+
+        print('All levels require ', paper_counter, ' pieces of paper')
+        return papers_list
 
 
 itemList = my_parser.parseXML('C:\\Users\\Инна\\Desktop\\Диплом\\SD_02856\\test')
-#print(itemList)
-#packAndShow(itemList, 841, 1189)
-pack(itemList, 841)
+print(itemList)
+
+print(packAndShow(itemList, 841, 1189))
